6f63d878067c538ad9c890dca5ac8f1b398ddc38
viet <phamhuuviet1998cx@gmail.com>
2025-06-29 00:57:03 +0700

    init commit

---
+++ client/src/layout/MainLayout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./MainLayout.module.css";
+
+function MainLayout() {
+  return <div className={Styles.MainLayout_container}></div>;
+}
+
+export default MainLayout;
